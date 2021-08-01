4b575ebbe081d2a55d35fbf3418903ab9d45cc8e
Ben D <bdek79@gmail.com>
2021-08-01 20:19:16 +0200

    Renders 20 Robots

--- src/components/Robots/Robots.tsx
+++ src/components/Robots/Robots.tsx
@@ -1,12 +1,13 @@
-import styles from './Robot.module.css';
+import styles from './Robots.module.css';
 import { Card } from '@material-ui/core';
+import Robot from '../Robot';
 
 export function Robots() {
   return (
     <>
-      <Card>
-        Robots
-      </Card>
+      {['1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5', '1', '2', '3', '4', '5'].map((i, index) => {
+        return <Robot key={index} />;
+      })}
     </>
   );
 }
