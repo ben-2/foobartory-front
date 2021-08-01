c395e78839783aae1ae4d5300fb236443c1d2910
Ben D <bdek79@gmail.com>
2021-08-01 23:00:29 +0200

    Refacto counters design

--- src/components/Counters/Counters.test.tsx
+++ src/components/Counters/Counters.test.tsx
@@ -1,27 +1,59 @@
 import { Counters } from './Counters';
-import { shallow } from 'enzyme';
+import { createStore, StoreProvider } from 'easy-peasy';
+import { mount } from 'enzyme';
+import { store } from '../../store';
 import CounterFoo from '../CounterFoo';
 import CounterBar from '../CounterBar';
 import CounterFooBar from '../CounterFooBar';
+import { useIntl } from 'react-intl';
+import LangProvider from '../../providers/LangProvider';
 
 describe('<Counters />', () => {
   it('match snapshot', () => {
-    const wrapper = shallow(<Counters />);
+    const initialStateForTest = { lang: 'fr' };
+    const storeTest = createStore(store, { initialState: initialStateForTest });
+    const wrapper = mount(
+      <StoreProvider store={storeTest}>
+        <LangProvider>
+          <Counters />
+        </LangProvider>
+      </StoreProvider>)
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders a Counter of Foo', () => {
-    const wrapper = shallow(<Counters />);
+    const initialStateForTest = { lang: 'fr' };
+    const storeTest = createStore(store, { initialState: initialStateForTest });
+    const wrapper = mount(
+      <StoreProvider store={storeTest}>
+        <LangProvider>
+          <Counters />
+        </LangProvider>
+      </StoreProvider>)
     expect(wrapper.find(CounterFoo).length).toEqual(1);
   });
 
   it('renders a Counter of Bar', () => {
-    const wrapper = shallow(<Counters />);
+    const initialStateForTest = { lang: 'fr' };
+    const storeTest = createStore(store, { initialState: initialStateForTest });
+    const wrapper = mount(
+      <StoreProvider store={storeTest}>
+        <LangProvider>
+          <Counters />
+        </LangProvider>
+      </StoreProvider>)
     expect(wrapper.find(CounterBar).length).toEqual(1);
   });
 
   it('renders a Counter of FooBar', () => {
-    const wrapper = shallow(<Counters />);
+    const initialStateForTest = { lang: 'fr' };
+    const storeTest = createStore(store, { initialState: initialStateForTest });
+    const wrapper = mount(
+      <StoreProvider store={storeTest}>
+        <LangProvider>
+          <Counters />
+        </LangProvider>
+      </StoreProvider>)
     expect(wrapper.find(CounterFooBar).length).toEqual(1);
   });
 });
